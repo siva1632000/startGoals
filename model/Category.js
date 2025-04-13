bb67fc61e0fe9b51c5eb297a31409d6ed188fb8c
siva1632000 <siva1632000@gmail.com>
2025-04-14 00:13:30 +0530

    category for course

---
+++ model/Category.js
@@ -0,0 +1,38 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db.js";
+import BaseModel from "./baseModel.js";
+
+class Category extends BaseModel {}
+
+Category.init(
+  {
+    categoryId: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    categoryName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    categoryCode: {
+      type: DataTypes.STRING(3),
+      allowNull: false,
+      unique: true,
+      validate: {
+        len: [2, 3],
+        isUppercase: true,
+      },
+    },
+    ...BaseModel.baseFields(), // ⬅️ Include base fields like createdAt, updatedAt, deletedAt
+  },
+  {
+    sequelize,
+    modelName: "Category",
+    tableName: "categories",
+    ...BaseModel.baseOptions(), // ⬅️ Include base options like paranoid & timestamps
+  }
+);
+
+export default Category;
--- routes/router.js
+++ routes/router.js
@@ -3,12 +3,13 @@ import userRoutes from "./userRoutes.js";
 import otpRoutes from "./otpRoutes.js";
 import languageRoutes from "./languageRoutes.js";
 import skillRoutes from "../routes/skillRoutes.js";
-
+import categories from "../routes/categoryRoutes.js";
 const router = express.Router();
 
 router.use(userRoutes);
 router.use(otpRoutes);
 router.use(languageRoutes);
 router.use(skillRoutes);
+router.use(categories);
 
 export default router;
